e3ef862dd6967bff077af870fb96b341bff7ff9d
slush <1208546972@qq.com>
2026-04-21 15:51:21 +0800

    修复按钮按下的状态的显示问题

--- scenes/ui/day_and_night_panel.gd
+++ scenes/ui/day_and_night_panel.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var time_label: Label = $PositionControl/TimePanel/MarginContainer/Time_Label
 @onready var day_label: Label = $PositionControl/DayPanel/MarginContainer/Day_Label
 
+@onready var normal_speed_button: Button = $PositionControl/Control/NormalSpeedButton
+@onready var cheetah_speed_button: Button = $PositionControl/Control/CheetahSpeedButton
+@onready var fast_speed_button: Button = $PositionControl/Control/FastSpeedButton
+
 @export var normal_speed:int = 5
 @export var fast_speed: int = 100
 @export var cheetah_speed: int =200
@@ -10,20 +14,31 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DayAndNightCycleManager.time_tick.connect(on_time_tick)
+	_sync_speed_button(DayAndNightCycleManager.game_speed)
 
 func on_time_tick(day: int, hour: int, minute: int) -> void:
 	day_label.text = "Day " + str(day)
 	time_label.text = "%02d:%02d" % [hour, minute]
 	
-
-
 func _on_normal_speed_button_pressed() -> void:
 	DayAndNightCycleManager.game_speed = normal_speed
+	_sync_speed_button(DayAndNightCycleManager.game_speed)
 
-
-func _on_fast_speed_button_2_pressed() -> void:
+func _on_fast_speed_button_pressed() -> void:
 	DayAndNightCycleManager.game_speed = fast_speed
+	_sync_speed_button(DayAndNightCycleManager.game_speed)
 
-
-func _on_cheetah_speed_button_3_pressed() -> void:
+func _on_cheetah_speed_button_pressed() -> void:
 	DayAndNightCycleManager.game_speed = cheetah_speed
+	_sync_speed_button(DayAndNightCycleManager.game_speed)
+	
+	
+func _sync_speed_button(now_speed: float) -> void:
+	normal_speed_button.set_pressed_no_signal(now_speed == normal_speed)
+	fast_speed_button.set_pressed_no_signal(now_speed == fast_speed)
+	cheetah_speed_button.set_pressed_no_signal(now_speed == cheetah_speed)
+	
+
+	
+	
+	
--- scenes/ui/tools_panel.gd
+++ scenes/ui/tools_panel.gd
@@ -1,9 +1,14 @@
 extends PanelContainer
+
 @onready var tool_axe: Button = $MarginContainer/HBoxContainer/ToolAxe
 @onready var tool_tilling: Button = $MarginContainer/HBoxContainer/ToolTilling
 @onready var tool_corn: Button = $MarginContainer/HBoxContainer/ToolCorn
 @onready var tool_tomato: Button = $MarginContainer/HBoxContainer/ToolTomato
 @onready var tool_watering_can: Button = $MarginContainer/HBoxContainer/ToolWateringCan
+
+func _ready() -> void:
+	ToolManager.tool_selected.connect(_on_tool_selected)
+	_sync_tool_buttons(ToolManager.selected_tool)
 
 func _on_tool_axe_pressed() -> void:
 	ToolManager.select_tool(DataTypes.Tools.AxeWood)
@@ -13,20 +18,26 @@ func _on_tool_tilling_pressed() -> void:
 
 func _on_tool_watering_can_pressed() -> void:
 	ToolManager.select_tool(DataTypes.Tools.WaterCrops)
-	
+
 func _on_tool_corn_pressed() -> void:
 	ToolManager.select_tool(DataTypes.Tools.PlantCron)
 
 func _on_tool_tomato_pressed() -> void:
 	ToolManager.select_tool(DataTypes.Tools.PlantTomato)
 
-#只有在未处理的输入处理被启用时，才会被调用
+func _on_tool_selected(tool: DataTypes.Tools) -> void:
+	_sync_tool_buttons(tool)
+
+#set_pressed_no_signal绑定按钮检查器中BaseButton的toggle Mode（为true）使用
+func _sync_tool_buttons(selected_tool: DataTypes.Tools) -> void:
+	tool_axe.set_pressed_no_signal(selected_tool == DataTypes.Tools.AxeWood)
+	tool_tilling.set_pressed_no_signal(selected_tool == DataTypes.Tools.TillGround)
+	tool_watering_can.set_pressed_no_signal(selected_tool == DataTypes.Tools.WaterCrops)
+	tool_corn.set_pressed_no_signal(selected_tool == DataTypes.Tools.PlantCron)
+	tool_tomato.set_pressed_no_signal(selected_tool == DataTypes.Tools.PlantTomato)
+
+#取消选中
 func _unhandled_input(event: InputEvent) -> void:
-	#如果给定的动作与该事件匹配且正被按下
+	# 这里的 release_tool 不是 Godot 内置关键字，而是你在 Input Map 里自己定义的动作名。它可能绑定了某个按键，比如 Esc、Q 之类。
 	if event.is_action_pressed("release_tool"):
 		ToolManager.select_tool(DataTypes.Tools.None)
-		tool_axe.release_focus()
-		tool_corn.release_focus()
-		tool_tilling.release_focus()
-		tool_tomato.release_focus()
-		tool_watering_can.release_focus()
